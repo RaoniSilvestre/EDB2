60aaeaad1d3bc39a05acb26688232a8383bf57a5
Pedro Galvão <90785456+Ordep-42@users.noreply.github.com>
2025-01-09 22:32:18 -0300

    feat: Adição de comentários ao código.

--- Unidade_02/ArvoreRubroNegra/include/arvore_rn.h
+++ Unidade_02/ArvoreRubroNegra/include/arvore_rn.h
@@ -1,11 +1,40 @@
 #pragma once
 
 #include <stdlib.h>
+/**
+ * @file arvore_rn.h
+ * @brief Definições de tipos para a implementação de uma Árvore Rubro-Negra.
+ */
 
+/**
+ * @enum cor_t
+ * @brief Enumeração para representar as cores dos nós na Árvore Rubro-Negra.
+ * 
+ * @var cor_t::RUBRO
+ * Nó de cor vermelha.
+ * @var cor_t::NEGRO
+ * Nó de cor preta.
+ */
 typedef enum cor_t {
   RUBRO, NEGRO
 } cor_t;
 
+
+/**
+ * @struct arvore_rn_t
+ * @brief Estrutura que representa um nó na Árvore Rubro-Negra.
+ * 
+ * @var:chave
+ * Chave associada ao nó.
+ * @var:cor
+ * Cor do nó, podendo ser RUBRO ou NEGRO.
+ * @var:esq
+ * Ponteiro para o nó filho à esquerda.
+ * @var:dir
+ * Ponteiro para o nó filho à direita.
+ * @var:pai
+ * Ponteiro para o nó pai.
+ */
 typedef struct arvore_rn_t {
   int chave;
   cor_t cor;
@@ -16,9 +45,6 @@ typedef struct arvore_rn_t {
 
 extern arvore_rn_t *raiz;
 
-// void inserir(arvore_rn_t **raiz, int chave);
-void corrigir_balanceamento(arvore_rn_t *no);
-arvore_rn_t *criar_no(int chave);
 arvore_rn_t *inserir(arvore_rn_t *arvore, arvore_rn_t *no);
 arvore_rn_t *remover(arvore_rn_t *arvore, int chave);
 arvore_rn_t *lista_p_arvore(int *chaves, int tamanho);
--- Unidade_02/ArvoreRubroNegra/src/arvore_rn.c
+++ Unidade_02/ArvoreRubroNegra/src/arvore_rn.c
@@ -7,6 +7,11 @@
 
 arvore_rn_t *raiz = NULL;
 
+/**
+ * Realiza uma rotação à esquerda na árvore rubro-negra.
+ *
+ * @param x Ponteiro para o nó onde a rotação será realizada.
+ */
 void rotacao_esq(arvore_rn_t *x) {
     arvore_rn_t *y = x->dir;
     x->dir = y->esq;
@@ -25,6 +30,11 @@ void rotacao_esq(arvore_rn_t *x) {
     x->pai = y;
 }
 
+/**
+ * Realiza uma rotação à direita na árvore rubro-negra.
+ *
+ * @param x Ponteiro para o nó onde a rotação será realizada.
+ */
 void rotacao_dir(arvore_rn_t *x) {
     arvore_rn_t *y = x->esq;
     x->esq = y->dir;
@@ -43,6 +53,11 @@ void rotacao_dir(arvore_rn_t *x) {
     x->pai = y;
 }
 
+/**
+ * Corrige o balanceamento da árvore rubro-negra após a inserção de um nó.
+ *
+ * @param no Ponteiro para o nó recém-inserido que pode ter causado o desbalanceamento.
+ */
 void corrigir_balanceamento(arvore_rn_t *no) {
     arvore_rn_t *pai = NULL;
     arvore_rn_t *avo = NULL;
@@ -97,6 +112,17 @@ void corrigir_balanceamento(arvore_rn_t *no) {
     raiz->cor = NEGRO;
 }
 
+/**
+ * @brief Insere um nó em uma árvore rubro-negra.
+ *
+ * Esta função insere um nó em uma árvore rubro-negra, mantendo a propriedade
+ * da árvore. Se a árvore estiver vazia, o nó inserido se torna a nova raiz.
+ * Caso contrário, a função insere o nó na posição correta com base na chave.
+ *
+ * @param arvore Ponteiro para a raiz da árvore rubro-negra.
+ * @param no Ponteiro para o nó a ser inserido.
+ * @return Ponteiro para a raiz da árvore rubro-negra após a inserção.
+ */
 arvore_rn_t *inserir(arvore_rn_t *arvore, arvore_rn_t *no) {
     if (arvore == NULL) {
         return no;
@@ -111,6 +137,16 @@ arvore_rn_t *inserir(arvore_rn_t *arvore, arvore_rn_t *no) {
     return arvore;
 }
 
+/**
+ * @brief Cria um novo nó para a árvore rubro-negra.
+ *
+ * Esta função aloca memória para um novo nó da árvore rubro-negra, inicializa
+ * sua chave, define sua cor como rubro, e inicializa os ponteiros pai, esquerdo
+ * e direito como NULL.
+ *
+ * @param chave A chave a ser armazenada no novo nó.
+ * @return Um ponteiro para o novo nó criado.
+ */
 arvore_rn_t *criar_no(int chave) {
     arvore_rn_t *novo_no = (arvore_rn_t *)malloc(sizeof(arvore_rn_t));
     novo_no->chave = chave;
@@ -121,6 +157,17 @@ arvore_rn_t *criar_no(int chave) {
     return novo_no;
 }
 
+/**
+ * @brief Busca um nó na árvore rubro-negra com a chave especificada.
+ *
+ * Esta função percorre a árvore rubro-negra recursivamente para encontrar
+ * um nó que contenha a chave fornecida. Se a chave for encontrada, o nó
+ * correspondente é retornado. Caso contrário, a função retorna NULL.
+ *
+ * @param arvore Ponteiro para a raiz da árvore rubro-negra.
+ * @param chave A chave a ser buscada na árvore.
+ * @return Ponteiro para o nó que contém a chave, ou NULL se a chave não for encontrada.
+ */
 arvore_rn_t *buscar(arvore_rn_t *arvore, int chave) {
     if (arvore == NULL) {
         return 0;
@@ -145,6 +192,19 @@ arvore_rn_t *find_min(arvore_rn_t *arvore) {
     return arvore;
 }
 
+/**
+ * Remove um nó com a chave especificada de uma árvore rubro-negra.
+ *
+ * @param arvore Ponteiro para a raiz da árvore rubro-negra.
+ * @param chave A chave do nó a ser removido.
+ * @return Ponteiro para a nova raiz da árvore após a remoção.
+ *
+ * A função remove o nó com a chave especificada da árvore rubro-negra,
+ * ajustando os ponteiros dos nós conforme necessário. Se a chave não
+ * for encontrada, a árvore original é retornada. Se o nó a ser removido
+ * tiver dois filhos, o nó com a menor chave na subárvore direita é
+ * encontrado e substitui o nó a ser removido.
+ */
 arvore_rn_t *remover(arvore_rn_t *arvore, int chave) {
     if (arvore == NULL) return NULL;
 
@@ -179,6 +239,15 @@ arvore_rn_t *lista_p_arvore(int *chaves, int tamanho) {
     return raiz;
 }
 
+/**
+ * @brief Desaloca a árvore rubro-negra recursivamente.
+ *
+ * Esta função percorre a árvore rubro-negra de forma recursiva e libera a memória
+ * alocada para cada nó da árvore.
+ *
+ * @param arvore Ponteiro para a raiz da árvore rubro-negra a ser desalocada.
+ * @return Retorna NULL após desalocar todos os nós da árvore.
+ */
 arvore_rn_t *desalocar(arvore_rn_t *arvore) {
     if (arvore != NULL) {
         arvore->esq = desalocar(arvore->esq);
@@ -188,6 +257,15 @@ arvore_rn_t *desalocar(arvore_rn_t *arvore) {
     return NULL;
 }
 
+/**
+ * @brief Realiza o percurso em ordem (inorder) em uma árvore rubro-negra.
+ *
+ * Esta função percorre a árvore rubro-negra em ordem, ou seja, visita
+ * recursivamente o nó da subárvore esquerda, o nó raiz e o nó da subárvore
+ * direita, nesta ordem. Durante a visita, a chave de cada nó é impressa.
+ *
+ * @param arvore Ponteiro para a árvore rubro-negra a ser percorrida.
+ */
 void inorder(arvore_rn_t *arvore) {
     if (arvore != NULL) {
         inorder(arvore->esq);
@@ -196,6 +274,19 @@ void inorder(arvore_rn_t *arvore) {
     }
 }
 
+/**
+ * @brief Imprime a sub-árvore da árvore rubro-negra de forma recursiva.
+ *
+ * Esta função imprime a sub-árvore da árvore rubro-negra, utilizando
+ * recursão para percorrer os nós da árvore. A impressão é feita de forma
+ * que a árvore é visualizada "deitada", com o nó raiz à esquerda e os nós
+ * filhos à direita. A cor dos nós é representada por cores de fundo no
+ * terminal: vermelho para nós vermelhos e preto para nós pretos.
+ *
+ * @param arvore Ponteiro para o nó raiz da sub-árvore a ser impressa.
+ * @param espaços Número de espaços a serem impressos antes do valor do nó,
+ *                utilizado para formatar a visualização da árvore.
+ */
 void imprimir_sub_arvore(arvore_rn_t *arvore, int espaços) {
     if (arvore != NULL) {
         espaços += 5;
@@ -214,7 +305,15 @@ void imprimir_sub_arvore(arvore_rn_t *arvore, int espaços) {
     }
 }
 
-void imprimir_arvore(arvore_rn_t *arvore) {
+/**
+ * @brief Imprime a árvore rubro-negra.
+ *
+ * Esta função imprime a árvore rubro-negra completa, chamando a função
+ * imprimir_sub_arvore para realizar a impressão recursiva dos nós.
+ *
+ * @param raiz Ponteiro para o nó raiz da árvore a ser impressa.
+ */
+void imprimir_arvore(arvore_rn_t *raiz) {
     printf("Visualização da árvore deitada:\n");
-    imprimir_sub_arvore(arvore, 0);
+    imprimir_sub_arvore(raiz, 0);
 }
