21cc0e4087ed9cf1bdfa0ac1198f1db5ec5e160a
Thiago F. <150091259+thigoai@users.noreply.github.com>
2025-01-09 21:28:01 -0300

    Documentação da Arvore Binária e mais (#10)
    * ...
    
    * Delete .replit
    
    * alterações na arvore de busca
    
    * Documentação da arvore binaria
    
    * Delete replit.nix
    
    * Delete .ccls-cache directory

--- Unidade_02/ArvoreDeBusca/include/arvore.h
+++ Unidade_02/ArvoreDeBusca/include/arvore.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct arvore_t {
   int chave;
@@ -8,8 +10,16 @@ typedef struct arvore_t {
 
 arvore_t *inserir(arvore_t *arvore, int chave);
 arvore_t *remover(arvore_t *arvore, int chave);
+arvore_t *construir_arvore(int *chaves, int inicio, int fim, arvore_t *arvore);
 arvore_t *lista_p_arvore(int *chaves, int tamanho);
 arvore_t *buscar(arvore_t *arvore, int chave);
-void inorder(arvore_t* arvore);
+
+void mostrarEmOrdem(arvore_t *arvore);
+void mostrarPreOrdem(arvore_t *arvore);
+void mostrarPosOrdem(arvore_t *arvore);
+
 
 arvore_t *desalocar(arvore_t *arvore);
+
+void gerar_dot(FILE *arquivo, arvore_t *raiz);
+void exportar_para_dot(arvore_t *raiz, const char *nome_arquivo);
